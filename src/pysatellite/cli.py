16b8065cfe349e6ebdcc1aa5d09dadef99ba4ed3
pySatellite <pysatellite@gmail.com>
2021-08-02 18:54:08 +0900

    인공위성 정보 수신 요구사항 작성

--- src/pysatellite/cli.py
+++ src/pysatellite/cli.py
@@ -22,3 +22,39 @@ def what_are_you_doing() -> str:
              "wlrn wndnlfmf ehfrh dlTdj. dh! djwlfjqek."]
 
     return random.choice(대답_뭉치)
+
+
+def where_are_you_now(satellite_id: int, api_key: str, observer_lat=37.44, observer_lng=126.96, observer_alt=592.0, seconds=1) -> dict:
+    """인공위성의 위도,경도,고도 등의 정보 수신
+
+    Args:
+        satellite_id: 인공위성 식별 id
+        api_key: api key
+        observer_lat: Observer's latitide (decimal degrees format)
+        observer_lng: Observer's longitude (decimal degrees format)
+        observer_alt: Observer's altitude above sea level in meters
+        seconds: Number of future positions to return. Each second is a position. Limit 300 seconds
+
+    Returns:
+        https://www.n2yo.com/api/#positions
+    """
+    return {
+        "info": {
+            "satname": "CAS500-1",
+            "satid": 47932,
+            "transactionscount": 14
+        },
+        "positions": [
+            {
+                "satlatitude": 72.37270065,
+                "satlongitude": -130.5659036,
+                "sataltitude": 507.55,
+                "azimuth": 20.41,
+                "elevation": -25.18,
+                "ra": 16.73985319,
+                "dec": 24.41137318,
+                "timestamp": 1627895784,
+                "eclipsed": False
+            }
+        ]
+    }
--- tests/pysatellite/test_cli.py
+++ tests/pysatellite/test_cli.py
@@ -1,5 +1,5 @@
-from pysatellite.cli import ping, what_are_you_doing
-
+from pysatellite.cli import ping, what_are_you_doing, where_are_you_now
+import os
 
 def test_ping():
     print(ping())
@@ -7,3 +7,10 @@ def test_ping():
 
 def test_what_are_you_doing():
     print(what_are_you_doing())
+
+
+def test_where_are_you_now():
+    # https://www.n2yo.com/login/edit/ -> License K
+    # export N2YO_KEY="****"
+    r = where_are_you_now(satellite_id=47932, api_key=os.environ['N2YO_KEY'])
+    assert r['info']['satname'] == 'CAS500-1'
